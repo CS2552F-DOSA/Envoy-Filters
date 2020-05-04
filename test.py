8ee0c64df51a579e323748dfbcb0e91f7c2df87d
Pastèque <519134691@qq.com>
2020-05-03 23:38:44 -0400

    Add a test python

---
+++ test.py
@@ -0,0 +1,64 @@
+# Reflects the requests from HTTP methods GET, POST, PUT, and DELETE
+# Written by Nathan Hamiel (2010)
+
+from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
+from optparse import OptionParser
+
+import sys
+
+count = 1
+
+class RequestHandler(BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        global count
+        request_path = self.path
+        
+        print("\n----- Request Start ----->\n")
+        print(request_path)
+        print(self.headers)
+        print("<----- Request End -----\n")
+        
+        self.send_response(200)
+        count += 1
+        self.send_header("fid_timestamp_unix_ns", str(count))
+        self.end_headers()
+        
+    def do_POST(self):
+        global count
+        request_path = self.path
+        
+        print("\n----- Request Start ----->\n")
+        print(request_path)
+        
+        request_headers = self.headers
+        content_length = request_headers.getheaders('content-length')
+        length = int(content_length[0]) if content_length else 0
+        
+        print(request_headers)
+        print(self.rfile.read(length))
+        print("<----- Request End -----\n")
+        
+        self.send_response(200)
+        count += 1
+        self.send_header("fid_timestamp_unix_ns", str(count))
+        self.end_headers()
+    
+    do_PUT = do_POST
+    do_DELETE = do_GET
+        
+def main():
+    port = int(sys.argv[1])
+    print('Listening on localhost:%s' % port)
+    server = HTTPServer(('', port), RequestHandler)
+    server.serve_forever()
+
+        
+if __name__ == "__main__":
+    parser = OptionParser()
+    parser.usage = ("Creates an http-server that will echo out any GET or POST parameters\n"
+                    "Run:\n\n"
+                    "   reflect")
+    (options, args) = parser.parse_args()
+    
+    main()
