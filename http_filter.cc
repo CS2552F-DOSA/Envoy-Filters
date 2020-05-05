619fbbea61142325842f52dbf858252ebbb77ddd
Pastèque <519134691@qq.com>
2020-05-05 15:01:17 -0400

    Update the naive code to be correct version

--- http_filter.cc
+++ http_filter.cc
@@ -18,9 +18,10 @@ static LowerCaseString FidTimestamp2{"fid_timestamp_unix_ns"};
 static LowerCaseString Method{":method"};
 static LowerCaseString URLPath{":path"};
 static LowerCaseString Host{":authority"};
+static LowerCaseString CL{"cluster"};
 
 DosaConfig::DosaConfig(const dosa::Dosa& proto_config, Upstream::ClusterManager& cm):
-  cm_(cm), cluster_(proto_config.cluster()) {}
+  cm_(cm), cluster_(proto_config.cluster()), prod_cluster_(proto_config.prod_cluster()) {}
 
 DosaEngine HttpSampleDecoderFilter::engine_ = DosaEngine();
 
@@ -34,6 +35,7 @@ void HttpSampleDecoderFilter::onDestroy() {}
 
 FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& headers, bool) {
   ENVOY_STREAM_LOG(info, "Dosa::decodeHeaders: {}", *decoder_callbacks_, headers);
+  
   if(headers.get(Method)->value() == "GET"
     && FilterState::Null == filter_state_){
     // We have a init get request, will send two ping requests
@@ -44,13 +46,8 @@ FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& hea
     RequestMessagePtr request1(new RequestMessageImpl(
       createHeaderMap<RequestHeaderMapImpl>(headers)));
     std::string oldURL = std::string(headers.get(URLPath)->value().getStringView());
-    request1->headers().setPath(std::string("/ping") + oldURL);
+    request1->headers().setPath(std::string("/1/ping") + oldURL);
     request1->headers().setHost(config_->cluster_); // cluster name is Host: header value!
-    // request->headers().insertMethod().value(Http::Headers::get().MethodValues.Post);
-    // request->headers().insertPath().value(std::string("/ambassador/auth"));
-    // request->headers().insertContentType().value(std::string("application/json"));
-    // request->headers().insertContentLength().value(request_body.size());
-    // request->body() = Buffer::InstancePtr(new Buffer::OwnedImpl(request_body));
     test_request_ =
         config_->cm_.httpAsyncClientForCluster(config_->cluster_)
             .send(std::move(request1), *this,
@@ -59,9 +56,9 @@ FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& hea
     RequestMessagePtr request2(new RequestMessageImpl(
       createHeaderMap<RequestHeaderMapImpl>(headers)));
     request2->headers().setPath(std::string("/ping") + oldURL);
-    request2->headers().setHost(std::string(headers.get(Host)->value().getStringView())); // cluster name is Host: header value!
+    request2->headers().setHost(config_->prod_cluster_);
     test_request_ =
-        config_->cm_.httpAsyncClientForCluster(std::string(headers.get(Host)->value().getStringView()))
+        config_->cm_.httpAsyncClientForCluster(config_->prod_cluster_)
             .send(std::move(request2), *this,
             AsyncClient::RequestOptions().setTimeout(std::chrono::milliseconds(5000)));
     
@@ -71,6 +68,13 @@ FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& hea
     && FilterState::GetDupSent == filter_state_){
 
     headers.setHost(cluster_);
+    // Modify the url
+    if(cluster_ == config_->cluster_){
+      // If we need to send to test
+      // NOTE: Hard code
+      std::string oldURL = std::string(headers.get(URLPath)->value().getStringView());
+      headers.setPath(std::string("/1") + oldURL);
+    }
     return FilterHeadersStatus::Continue;
 
   }else if(headers.get(Method)->value() == "POST"
@@ -83,9 +87,9 @@ FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& hea
       createHeaderMap<RequestHeaderMapImpl>(headers)));
     std::string oldURL = std::string(headers.get(URLPath)->value().getStringView());
     request->headers().setPath(std::string("/ping") + oldURL);
-    request->headers().setHost(std::string(headers.get(Host)->value().getStringView()));
+    request->headers().setHost(config_->prod_cluster_);
     test_request_ =
-        config_->cm_.httpAsyncClientForCluster(std::string(headers.get(Host)->value().getStringView()))
+        config_->cm_.httpAsyncClientForCluster(config_->prod_cluster_)
             .send(std::move(request), *this,
             AsyncClient::RequestOptions().setTimeout(std::chrono::milliseconds(5000)));
 
@@ -97,7 +101,9 @@ FilterHeadersStatus HttpSampleDecoderFilter::decodeHeaders(RequestHeaderMap& hea
     // use time stamp information and Send the write
     headers.addCopy(FidTimestamp2, test_reponse_time_);
     headers.setCopy(FidTimestamp2, test_reponse_time_);
-    headers.setHost(config_->cluster_);
+    // NOTE: Hard code
+    std::string oldURL = std::string(headers.get(URLPath)->value().getStringView());
+    headers.setPath(std::string("/1") + oldURL);
 
     return FilterHeadersStatus::Continue;
 
@@ -161,55 +167,22 @@ void HttpSampleDecoderFilter::onSuccess(const AsyncClient::Request&, ResponseMes
     && filter_state_ == FilterState::GetDupSent){
       if(test_reponse_time_ == ""){
         test_reponse_time_ = std::string(response->headers().get(FidTimestamp2)->value().getStringView());
-        cluster_ = std::string(response->headers().get(Host)->value().getStringView());
+        cluster_ = std::string(response->headers().get(CL)->value().getStringView());
       } else {
         // compare the time
         std::string new_reponse_time = std::string(response->headers().get(FidTimestamp2)->value().getStringView());
         if(std::stol(test_reponse_time_) <= std::stol(new_reponse_time)){
-          cluster_ = std::string(response->headers().get(Host)->value().getStringView());
+          // cluster_ = std::string(request->cluster_->name());
+          cluster_ = std::string(response->headers().get(CL)->value().getStringView());
         }
         decoder_callbacks_->continueDecoding();
       }
-  } else if(filter_type_ == FilterType::Get
+  } else if(filter_type_ == FilterType::Post
     && filter_state_ == FilterState::PostSent){
+      
       test_reponse_time_ = std::string(response->headers().get(FidTimestamp2)->value().getStringView());
       decoder_callbacks_->continueDecoding();
   }
-  // if(filter_type_ == FilterType::Get){
-  //   test_request_ = nullptr;
-
-  //   // store the package
-  //   // TODO: can be improved
-  //   test_reponse_time_ = std::string(response->headers().get(FidTimestamp)->value().getStringView());
-  //   // TODO: can be improved
-  //   test_response_body_ = response->bodyAsString();
-  //   if (Http::Utility::getResponseStatus(response->headers()) != enumToInt(Http::Code::OK)) {
-  //     ENVOY_LOG(info, "The dup request recvs a not 200");
-  //   } else {
-  //     ENVOY_LOG(info, "The dup request recvs a 200");
-  //   }
-  //   // std::unique_lock<std::mutex> lck(mtx_);
-  //   // if(filter_state_ == FilterState::GetDupWait){
-  //   //   filter_state_ = FilterState::GetDupRecv;
-  //   //   cv_.notify_all();
-  //   // }
-  //   // filter_state_ = FilterState::GetDupRecv;
-  //   // lck.unlock();
-  // } else {
-  //   // test_request_ = nullptr;
-  //   // std::unique_lock<std::mutex> lck(mtx_);
-  //   // filter_state_ = FilterState::PostDupRecv;
-  //   // cv_.notify_all();
-  //   // lck.unlock();
-
-  //   // // TODO: store the timestamp
-  //   // // test_response_body_ = response->bodyAsString();
-  //   // if (Http::Utility::getResponseStatus(response->headers()) != enumToInt(Http::Code::OK)) {
-  //   //   ENVOY_LOG(info, "The dup request recvs a not 200");
-  //   // } else {
-  //   //   ENVOY_LOG(info, "The dup request recvs a 200");
-  //   // }
-  // }
 
   ENVOY_LOG(info, "onSuccess returned");
   return;
